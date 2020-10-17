c4851e410f287d1cc2a29521e3635fb0b98379c3
manuelsanchez2 <manusansan22@gmail.com>
2020-10-17 22:18:39 +0200

    Set up mysql with server

--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const mysql = require("mysql");
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   user: "root",
   host: "localhost",
-  password: "password",
+  password: process.env.MYSQL_PASSWORD,
   database: "LoginSystem",
+});
+
+app.get("/", (req, res) => {
+  const sqlInsert =
+    "INSERT INTO users (user, password) VALUES ('pepe', 'pelicula');";
+  db.query(sqlInsert, (error, result) => {
+    res.send("Hola Manu, SQL works");
+  });
 });
 
 app.listen(3001, () => {
