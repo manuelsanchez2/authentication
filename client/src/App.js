f10c5c1a69e2e17dc13e8d26d1492f3c6d1cefa0
manuelsanchez2 <manusansan22@gmail.com>
2020-10-18 08:01:03 +0200

    Add dependencies and changes to setup register

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,34 @@
 import React, { useState } from "react";
 import "./App.css";
+import Axios from "axios";
 
 function App() {
   const [usernameRegister, setUsernameRegister] = useState("");
   const [passwordRegister, setPasswordRegister] = useState("");
+
+  const [usernameLogin, setUsernameLogin] = useState("");
+  const [passwordLogin, setPasswordLogin] = useState("");
+
+  const [loginStatus, setLoginStatus] = useState("");
+
+  const register = () => {
+    Axios.post("http://localhost:3001/register", {
+      username: usernameRegister,
+      password: passwordRegister,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
+  const login = () => {
+    Axios.post("http://localhost:3001/login", {
+      username: usernameLogin,
+      password: passwordLogin,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
   return (
     <div className="App">
       <h1>How to: register and login</h1>
@@ -24,13 +49,25 @@ function App() {
               setPasswordRegister(e.target.value);
             }}
           />
-          <button>Register</button>
+          <button onClick={register}>Register</button>
         </div>
         <div className="login">
           <h2>Login</h2>
-          <input type="text" placeholder="username..." />
-          <input type="password" placeholder="password..." />
-          <button>Login</button>
+          <input
+            type="text"
+            placeholder="username..."
+            onChange={(e) => {
+              setUsernameLogin(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            placeholder="password..."
+            onChange={(e) => {
+              setPasswordLogin(e.target.value);
+            }}
+          />
+          <button onClick={login}>Login</button>
         </div>
       </main>
     </div>
